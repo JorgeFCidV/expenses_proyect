6eb079ce6b0c4fc66c80214c87a67740b0fb52de
JorgeFCidV <jfc@cemad.es>
2025-12-02 13:46:38 +0000

    Your branch is up to date with 'origin/dev'. 	modified:   ../.env 	modified:   ../__pycache__/config.cpython-312.pyc 	modified:   __init__.py 	modified:   __pycache__/__init__.cpython-312.pyc 	modified:   __pycache__/email.cpython-312.pyc 	modified:   __pycache__/forms.cpython-312.pyc

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from config import Config
 import os
 
 db = SQLAlchemy()
+migrate = Migrate() 
 login_manager = LoginManager()
 login_manager.login_view = 'routes.login'
 login_manager.login_message_category = 'info'
@@ -14,6 +16,7 @@ def create_app():
     app.config.from_object(Config)
 
     db.init_app(app)
+    migrate.init_app(app, db)
     login_manager.init_app(app)
 
     # Importamos los modelos aquí para evitar import circular
